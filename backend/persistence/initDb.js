9059045111e63801dfe17692addb84f3701608b2
Chinemeremchigbo <chinemeremchigbo@outlook.com>
2021-01-24 16:43:00 -0500

    Next step

--- backend/persistence/initDb.js
+++ backend/persistence/initDb.js
@@ -8,7 +8,7 @@ import Restaurant from "./models/restaurant";
 import { SEED_RESTAURANT_DATA } from "./seedRestaurantData";
 
 /* change to true if you would like to seed the DB with test restaurant data (will drop any existing data) */
-const SEED_DB = true;
+const SEED_DB = false;
 
 export function connectDb() {
     /* mongoose provides abstracted methods for interacting with MongoDB, like connecting to the database */
